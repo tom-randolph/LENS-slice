566b625656a515aed810cedb5071ab626661944d
Tom Randolph <trandolph@umass.edu>
2018-05-31 13:30:50 -0400

    Implemented standard file IO, more robuse rules checking, argparse

--- post_process.py
+++ post_process.py
@@ -6,7 +6,7 @@
 
 import sys
 import os
-import fileinput
+import argparse
 
 
 
@@ -17,13 +17,15 @@ omit_line_rules=["G92", #extruder offset command
 					#"G1 E" ,#extruder specific commands
 					"M82", 
 					]
-lines_changed=0;
 
-postamble="""M5 ;turn off laser
-M303 ; turn off powder 1
-M305 ;turn off powder 2
-G1 Z20 F5000 ;Raise print head
-G28 X Y ; home X Y axis"""
+
+def omit(line):
+	return ';Line omitted in post-process\n'
+rules = {
+	'G92' : omit,
+	'M1' : omit,
+	'M82' : omit,
+}
 					
 def check_omit_line(line):
 	'''Checks to see if line contains bad codes according to omit_line_rules,
@@ -34,44 +36,87 @@ def check_omit_line(line):
 			
 	else: return False
 
-if __name__=="__main__":
+def parse_file(in_file,out_file):
+	'''Parse an input file line-by-line and change codes in file according to specified rule modifiers'''
 
-	nargs=len(sys.argv)
-	if nargs is not 2:
-		print("please provide a file to edit")
-		exit()
-	filename=sys.argv[1]
-	filename_, filetype=os.path.splitext(filename)
+	if os.path.isfile(out_file):
+		print('File already exists...')
+
+		ans = input('Would you like to overwrite it? (y/n)')
+		if ans in ['y','Y','Yes','yes']:
+			pass
+		else: 
+			print('No edits will be made. Specify output filename if desired.')
+			return None
+
+	# A counter for the nubmer of lines changed in the script
+	lines_changed=0;
+
+	# Open the input file
+	with open(in_file) as i_f:
+
+		# Store its contents
+		lines=i_f.readlines()
+
+		#Open and/or create the output file
+		with open(out_file, 'w') as o_f:
+
+			# For each line in the input file, apply the appropriate changes and write them to the output file
+			for line in lines:
+				was_changed, line = check_rules(line)
+				
+				# Track the number of changes made
+				if was_changed :
+					lines_changed += 1
+				o_f.write(line)
+
+	# Return the lines changed to an output statistic
+	return lines_changed
+
+def check_rules(in_line):
+	''' Checks to see which rule modifiers apply to a line of text and modifies them according to the fucntion specified in the dict'''
+
+	was_changed = False
+	out_line = in_line
+	for rule in rules:
+		# print(rule)
+		if rule in in_line:
+			was_changed = True
+			out_line = rules[rule](in_line)
+	# print(out_line)
+	return was_changed, out_line
+	
+
+if __name__=="__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument('input_file', help = 'the path to the input file', type=str)
+	parser.add_argument('output_file', help = 'the path to the output file', type=str, default = None, nargs='?')
+
+	args = parser.parse_args()
+
+	input_file=args.input_file
+	filename_, filetype =os.path.splitext(input_file)
 
 	if filetype not in supported_types:
 		print("Unrecognized filetype")
 		print("Please use the following types:")
 		print(*supported_types, sep=',')
-		exit()
+		sys.exit()
+
+	output_file=args.output_file
+
+	if output_file is None:
+		output_file = input_file.split('.')
+		output_file = output_file[0]+'-LENS.'+output_file[1]
 		
-	print("Processing {}....".format(filename))
+	print("Processing {}....".format(input_file))
+
+	lines_changed = parse_file(input_file,output_file)
 	
-	try:
-		#wrapping this operation in a try statement allows for automatic file recovery in case of a fatal error
-		with fileinput.FileInput(filename, inplace=True,backup ='.old') as file:
-		
-			for line in file:
-				if check_omit_line(line):
-					lines_changed+=1
-					continue
-				print(line,end='')
-		
-			
-	except:
-		#restore the original file from backup
-		os.remove(filename)
-		os.rename(filename+'.old',filename)
-		raise
-	#with open(filename,'a') as file:
-	#	file.write(postamble)
-	print("Finished")
-	print("Number of lines change:{}".format(lines_changed))
-	#print("Added Postamble:")
-	#print(postamble)
+	if lines_changed is not None:
+
+		print("Finished")
+		print("Number of lines changed: {}".format(lines_changed))
+
 	
 		
