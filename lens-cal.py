a94152ab766f08d4b3c95005ec6dfd0b4a122bf1
tom-randolph <tom.randolph@formlabs.com>
2018-06-24 18:54:04 -0400

    OO the whole thing, man. Easier to follow

--- lens-cal.py
+++ lens-cal.py
@@ -1,86 +1,161 @@
 import os
 import numpy as np
+import sys
 
-# class Test():
-#     max_id = -1
-#     origin = (0.0,0.0,0.0)
 
-#     __init__(self, params,dir=None):
-#         if dir is not None:
-#             self.dir=dir
-#         self.params = params
+class Print():
 
-#         self.var_x = self.params['x_axis']['var']
-#         self.var_y = self.params['y_axis']['var']
+    id = -1
+    origin = np.array((0,0,0))
 
-#         self.inc_x = self.params['x_axis']['increment']
-#         self.inc_y = self.params['y_axis']['increment']
+    def __init__(self, layer_height = .01, layers = 10, layer_dict = None, dir = 'tests/'):
+        Print.update_id(dir)
+        self.id = Print.id
+        self.pos = Print.origin.copy()
+        self.layer_height = layer_height
+        self.num_layers = layers
+        self.layers = []
+        self.layer_dict = layer_dict
+        self.generate_layers(layer_dict.copy())
+        self.dir = dir
 
-#         self.init_x = self.params[var_x]
-#         self.init_y = self.params[var_y]
 
-#         self.stride_x = self.params['stride_x']
-#         self.stride_y = self.params['stride_y']
-#         self.stride_z = self.params['stride_z']
-#         self.layers = self.params['layers']
-#         self.line_length = self.params['line_length']
-#         self.pos = np.array(origin)
 
-#     def generate_gcode(self):
-#         lines = []
-#         if self.params['scheme'] is 'line-grid-y':
-#         count = 0
+    def generate_layers(self, layer_dict):
 
-#         self.params[var_x] = init_x
-#         for layer in range(self.layers):
-#             layer = []
-#             while pos[0] < bed_dims[0]-stride_x:
-#                 column = []
-#                 self.params[var_y] = init_y
-#                 add_gcode_column(gcode_name, pos, power = self.params['laser_power_W'],
-#                                                  feed = self.params['feed_rate_ipm'], 
-#                                                  powder = self.params['powder_feed_rpm'])
-#                 while pos[1] <bed_dims[1]-stride_y:
-#                     point_1 = np.around(pos,4)
-#                     pos[1]+=line_length
-#                     point_2 = np.around(pos,4)
-#                     pos[1] = point_1[1] + stride_y
-#                     line = tuple((tuple(point_1),tuple(point_2)))
-#                     add_gcode_line(gcode_name, line, power = self.params['laser_power_W'],
-#                                                  feed = self.params['feed_rate_ipm'], 
-#                                                  powder = self.params['powder_feed_rpm'])
-                    
-#                     column.append(line)
-#                     self.params[var_y] += inc_y
+        for _ in range(self.num_layers):
+            self.layers.append(Layer(self.pos,**layer_dict))
+            self.pos = self.pos+np.array((0, 0, self.layer_height))
 
-#                 layer.append(column)
-#                 self.params[var_x] += inc_x
 
-#                 pos[0] += stride_x
-#                 pos[1] = origin[0]
-#             lines.append(layer) 
-#             pos[2] += stride_z
-#             pos[0] = origin[0]
-#             pos[1] = origin[1]
+    def as_gcode(self):
+        gcode = '(Print ' + str(self.id) + ')\n'
+        for layer in self.layers:
+            gcode += layer.as_gcode()
+        return gcode
 
-#         return lines
+    def as_dict(self):
+        _dict = {'Print': self.id,
+                'Number of Layers': len(self.layers),
+                'Layer Height': self.layer_height,
+                'Layers': [layer.as_dict() for layer in self.layers],
+        }
+        return _dict
+
+    def as_json(self):
+        import json
+        return json.dumps(self.as_dict())
+
+    def update_id(dir):
+        _files = []
+        for (dir_path, dir_names, file_names) in os.walk(dir):
+            _files+=(file_names)
+
+        json_files = [file for file in _files if file.split('.')[-1]=='json']
+        max_id = 0
+        for f in json_files:
+            id=int(f.split('.')[0].split('_')[-1])
+            if id > max_id:
+                max_id = id
+
+        Print.id=max_id+1
     
-    # def get_test_id(self):
-    #     _files = []
-    #     for (dir_path, dir_names, file_names) in os.walk(self.dir):
-    #         _files+=(file_names)
+    def write_gcode_file(self, dir = None):
+        
+        if dir is None:
+            dir = self.dir
 
-    #     json_files = [file for file in _files if file.split('.')[-1]=='json']
+        if not os.path.exists(dir):
+            os.mkdir(dir)
 
+        self.gcode_file = os.path.join(dir, 'test_' + str(self.id) + '.gcode')
+        if os.path.isfile(self.gcode_file):
+            print('A gcode file for this test number already exists...')
+            ans = input('Would you like to overwrite it? (y/n)')
+            if ans in 'Yesyes':
+                pass
+            else: 
+                print('No edits will be made. Specify output filename if desired.')
+                sys.exit(1)
+        with open(self.gcode_file, 'w')  as f:
+            f.write("""G17 (XY Plane Select)
+G20 (Use Inch Units, G21 is to use mm units)
+G40 (Turn off cutter radius compensation)
+G80 (Cancel canned cycles)
+G90 (Use absolute distance mode.  G91 is to use relative distance mode)
+G94 (Set feed rate - needs to have an "F" word after it)
+G49 (Do NOT use tool length offset)
+""")
+            f.write(self.as_gcode())
+        
+    def write_json_file(self, dir = None):
+        if dir is None:
+            dir = self.dir
+
+        if not os.path.exists(dir):
+            os.mkdir(dir)
+
+        self.json_file = os.path.join(dir, 'test_' + str(self.id) + '.json')
+        if os.path.isfile(self.json_file):
+            print('A json file for this test number already exists...')
+            ans = input('Would you like to overwrite it? (y/n)')
+            if ans in 'Yesyes':
+                pass
+            else: 
+                print('No edits will be made. Specify output filename if desired.')
+                sys.exit(1)
+        with open(self.json_file, 'w') as f:
+            f.write(self.as_json())
+    def write_files(self, dir = None):
+        self.write_json_file(dir)
+        self.write_gcode_file(dir)
         
 
-    #     for f in json_files:
-    #         id=int(f.split('.')[0].split('_')[-1])
-    #         if id > max_id:
-    #             max_id = id
-        
-    #     self.id=max_id+1
 
+class Layer():
+
+    id = 0
+    origin = np.array((0,0,0))
+
+    def __init__(self, start = None, stride = .24, width = 3, column_dict = None, var = None, inc = 0):
+        if start is None:
+            self.pos = Layer.origin.copy()
+        else: self.pos = start
+        self.stride = stride
+        self.width = width
+        self.columns = []
+        self.column_dict = column_dict
+        self.var = var
+        self.inc = inc
+        self.generate_columns(column_dict.copy())
+    
+    def generate_columns(self,column_dict):
+        while self.pos[0]< self.width - self.stride:
+            self.columns.append(Column(self.pos,**column_dict))
+            self.pos = self.pos+np.array((self.stride, 0, 0))
+            column_dict['line_dict'][self.var] += self.inc
+
+    def as_gcode(self):
+        gcode = '(Layer ' + str(self.id) + ' of stride ' + str(self.stride) + ')\n'
+        for column in self.columns:
+            gcode += column.as_gcode()
+        return gcode
+
+    def as_dict(self):
+        _dict = {'Layer': self.id,
+                'Stride': self.stride,
+                'Columns': [column.as_dict() for column in self.columns],
+                'Variable': self.var,
+                'Increment': self.inc
+
+        }
+        return _dict
+
+    def as_json(self):
+        import json
+        return json.dumps(self.as_dict())
+    
+    
 class Column():
     id = 0
 
@@ -94,22 +169,23 @@ class Column():
         Column.id+=1
         self.var = var
         self.inc = inc
-        self.generate_lines(line_dict)
+        self.generate_lines(line_dict.copy())
         
         
     def generate_lines(self,line_dict):
         while self.pos[1]< self.length - self.stride:
             self.lines.append(Line(self.pos,**line_dict))
             self.pos = self.pos+np.array((0,self.stride,0))
-            print('stride+pos',self.pos[1] + self.stride)
-            print('y:',self.pos[1])
             line_dict[self.var] += self.inc
-            print(self.pos)
+
+
+
 
     def as_gcode(self):
-        gcode = '(Column ' + str(self.id) + 'of stride' + str(self.stride) + ')\n'
+        gcode = '(Column ' + str(self.id) + ' of stride ' + str(self.stride) + ')\n'
         for line in self.lines:
             gcode += line.as_gcode()
+        return gcode
     def as_dict(self):
         _dict = {'Column': self.id,
                 'Stride': self.stride,
@@ -118,6 +194,7 @@ class Column():
                 'Increment': self.inc
 
         }
+        return _dict
     def as_json(self):
         import json
         return json.dumps(self.as_dict())
@@ -127,9 +204,9 @@ class Line():
     id = 0
 
     def __init__(self, start, length=.25, power = 200, powder = 4, feed = 6):
-        self.start = start
+        self.start = np.around(start,2)
         self.length = length
-        self.end = self.start + np.array((0,self.length,0))
+        self.end = np.around(start + np.array((0,self.length,0)),2)
         self.id = Line.id
         Line.id+=1
         self.power = power
@@ -137,11 +214,11 @@ class Line():
         self.feed = feed
     
     def as_gcode(self):
-        gcode = '(Line ' + str(self.id) + 'of length' + str(self.length) + ')\n'
+        gcode = '(Line ' + str(self.id) + ' of length ' + str(self.length) + ')\n'
         gcode += 'M302 P' + str(self.powder) + '\n'
-        gcode += 'G1 X'+str(self.start[0]) + ' Y' + str(self.start[1]) + ' Z' + str(self.start[2])+ ' F' + str(self.feed)
+        gcode += 'G1 X'+str(self.start[0]) + ' Y' + str(self.start[1]) + ' Z' + str(self.start[2])+ ' F' + str(self.feed) + '\n'
         gcode += '(Laser On)\n' + 'M3 S' + str(self.power)+'\n'
-        gcode += 'G1 X'+str(self.end[0]) + ' Y' + str(self.end[1])+ ' Z' + str(self.end[2])
+        gcode += 'G1 X'+str(self.end[0]) + ' Y' + str(self.end[1])+ ' Z' + str(self.end[2]) + '\n'
         gcode += '(Laser Off)\n' + 'M5' + '\n'
         return gcode
 
@@ -176,33 +253,38 @@ class Line():
 
 
 if __name__ == '__main__':
-    origin = np.array([0,0,0])
-    column_dict = {'var':'power',
-                    'inc':50,
-                    'stride':.5,
-                    'length':3,
-                    'line_dict':{
-                        'power':200,
-                        'powder':3,
-                        'feed':6,
-                        'length':.25,
+
+    print_params = {
+    'layer_height': .01,
+    'layers': 10,
+    'layer_dict': 
+        {
+        'var':'powder',
+        'inc': .6,
+        'stride': .7,
+        'width' : 3.4,
+        'column_dict' : 
+            {
+            'var':'power',
+            'inc':100,
+            'stride':.6,
+            'length':3.2,
+            'line_dict':
+                {
+                'power':220,
+                'powder':3.3,
+                'feed':2.2,
+                'length':.22,
                         
-                        },
+                },
 
-                    }
+            },
+        },
+    }
 
+
+    print_one = Print(**print_params)
+    # print(print_one.layers)
+    print_one.write_files()
     
-    column = Column(origin,**column_dict)
-
-    line_dict={
-                        'power':200,
-                        'powder':3,
-                        'feed':6,
-                        'length':.25,
-                        
-                        }
-
-    line = Line(origin,**line_dict)
-
-    with open('test.json','w') as f:
-        f.write(column.as_json())
+    
